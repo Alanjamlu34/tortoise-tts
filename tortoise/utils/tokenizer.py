81e833e080c5d8f1cd8fb74d85a2888aaa9b9595
SandiJamlu <sandijamlu694@gmail.com>
2024-07-22 11:50:40 +0800

    update tokenizer

--- tortoise/utils/tokenizer.py
+++ tortoise/utils/tokenizer.py
@@ -1,18 +1,12 @@
 import os
 import re
-
 import inflect
 import torch
 from tokenizers import Tokenizer
-
-
-# Regular expression matching whitespace:
 from unidecode import unidecode
 
 _whitespace_re = re.compile(r'\s+')
 
-
-# List of (regular expression, replacement) pairs for abbreviations:
 _abbreviations = [(re.compile('\\b%s\\.' % x[0], re.IGNORECASE), x[1]) for x in [
   ('g', 'enggak'),
   ('ga', 'enggak'),
@@ -24,29 +18,23 @@ _abbreviations = [(re.compile('\\b%s\\.' % x[0], re.IGNORECASE), x[1]) for x in 
   ('yg', 'yang')
 ]]
 
-
 def expand_abbreviations(text):
   for regex, replacement in _abbreviations:
     text = re.sub(regex, replacement, text)
   return text
 
-
 _inflect = inflect.engine()
 _comma_number_re = re.compile(r'([0-9][0-9\,]+[0-9])')
 _decimal_number_re = re.compile(r'([0-9]+\.[0-9]+)')
-_pounds_re = re.compile(r'£([0-9\,]*[0-9]+)')
-_dollars_re = re.compile(r'\$([0-9\.\,]*[0-9]+)')
+_dollars_re = re.compile(r'Rp([0-9\.\,]*[0-9]+)')
 _ordinal_re = re.compile(r'[0-9]+(st|nd|rd|th)')
 _number_re = re.compile(r'[0-9]+')
-
 
 def _remove_commas(m):
   return m.group(1).replace(',', '')
 
-
 def _expand_decimal_point(m):
-  return m.group(1).replace('.', ' point ')
-
+  return m.group(1).replace('.', ' koma ')
 
 def _expand_dollars(m):
   match = m.group(1)
@@ -57,21 +45,18 @@ def _expand_dollars(m):
   cents = int(parts[1]) if len(parts) > 1 and parts[1] else 0
   if rupiah and cents:
     rupiah_unit = 'rupiah' if rupiah == 1 else 'rupiah'
-    cent_unit = 'cent' if cents == 1 else 'cents'
+    cent_unit = 'sen' if cents == 1 else 'sen'
     return '%s %s, %s %s' % (rupiah, rupiah_unit, cents, cent_unit)
   elif rupiah:
-    rpiah_unit = 'dollar' if rupiah == 1 else 'dollars'
-    return '%s %s' % (rupiah, rupiah)
+    return '%s rupiah' % (rupiah)
   elif cents:
-    cent_unit = 'cent' if cents == 1 else 'cents'
+    cent_unit = 'sen' if cents == 1 else 'sen'
     return '%s %s' % (cents, cent_unit)
   else:
     return 'nol rupiah'
 
-
 def _expand_ordinal(m):
-  return _inflect.number_to_words(m.group(0))
-
+  return _inflect.number_to_words(m.group(0), lang='id')
 
 def _expand_number(m):
   num = int(m.group(0))
@@ -79,40 +64,33 @@ def _expand_number(m):
     if num == 2000:
       return 'dua ribu'
     elif num > 2000 and num < 2010:
-      return 'dua ribu ' + _inflect.number_to_words(num % 100)
+      return 'dua ribu ' + _inflect.number_to_words(num % 100, lang='id')
     elif num % 100 == 0:
-      return _inflect.number_to_words(num // 100) + ' ratus'
+      return _inflect.number_to_words(num // 100, lang='id') + ' ratus'
     else:
-      return _inflect.number_to_words(num, andword='', zero='oh', group=2).replace(', ', ' ')
+      return _inflect.number_to_words(num, andword='', zero='oh', group=2, lang='id').replace(', ', ' ')
   else:
-    return _inflect.number_to_words(num, andword='')
-
+    return _inflect.number_to_words(num, andword='', lang='id')
 
 def normalize_numbers(text):
   text = re.sub(_comma_number_re, _remove_commas, text)
-  text = re.sub(_pounds_re, r'\1 pounds', text)
   text = re.sub(_dollars_re, _expand_dollars, text)
   text = re.sub(_decimal_number_re, _expand_decimal_point, text)
   text = re.sub(_ordinal_re, _expand_ordinal, text)
   text = re.sub(_number_re, _expand_number, text)
   return text
 
-
 def expand_numbers(text):
   return normalize_numbers(text)
-
 
 def lowercase(text):
   return text.lower()
 
-
 def collapse_whitespace(text):
   return re.sub(_whitespace_re, ' ', text)
 
-
 def convert_to_ascii(text):
   return unidecode(text)
-
 
 def basic_cleaners(text):
   '''Basic pipeline that lowercases and collapses whitespace without transliteration.'''
@@ -120,14 +98,12 @@ def basic_cleaners(text):
   text = collapse_whitespace(text)
   return text
 
-
 def transliteration_cleaners(text):
   '''Pipeline for non-English text that transliterate to ASCII.'''
   text = convert_to_ascii(text)
   text = lowercase(text)
   text = collapse_whitespace(text)
   return text
-
 
 def english_cleaners(text):
   '''Pipeline for English text, including number and abbreviation expansion.'''
@@ -138,7 +114,6 @@ def english_cleaners(text):
   text = collapse_whitespace(text)
   text = text.replace('"', '')
   return text
-
 
 def lev_distance(s1, s2):
   if len(s1) > len(s2):
@@ -155,9 +130,7 @@ def lev_distance(s1, s2):
     distances = distances_
   return distances[-1]
 
-
 DEFAULT_VOCAB_FILE = os.path.join(os.path.dirname(os.path.realpath(__file__)), '../data/custom_language_tokenizer.json')
-
 
 class VoiceBpeTokenizer:
     def __init__(self, vocab_file=None, use_basic_cleaners=False):
